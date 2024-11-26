8767fbf338888396cda1dc551fa7ec5d443c06ce
Bukobza <rom@grei.club>
2024-11-26 10:05:39 +0200

    Add rest of comments requests

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const commentsController = require('../controllers/commentsController');
+const commentsController = require('../../controllers/comments/comments-controller');
 
 const commentsRouter = express.Router();
 
 commentsRouter.get('/', commentsController.getAllComments);
+
+commentsRouter.get('/:comment_id', commentsController.getCommentById);
+
+commentsRouter.post('/', commentsController.createComment);
+
+commentsRouter.put('/:comment_id', commentsController.updateComment);
 
 module.exports = commentsRouter;
